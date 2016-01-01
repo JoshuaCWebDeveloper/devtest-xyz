536d9b97ff952d5e5932b3aab405e80a4ee37b9b
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2015-12-31 18:51:22 -0800

    Allow form value to be passed to Select and Input React components

--- public/app/form/shared/Input.js
+++ public/app/form/shared/Input.js
@@ -9,17 +9,45 @@
 import React from 'react';
 //create Input react component
 var Input = class extends React.Component {
+    constructor (props) {
+        super(props);
+        //set initial state
+        this.state = {
+            changed: false,
+            value: this.props.value
+        };
+    }
+    
     render () {
-        var className = "input-group";
+        //set value of our input
+        var val = this.state.value,
+            className = "input-group";
         if (this.props.float) {
             className += " " + this.props.float;
+        }
+        //if our state's value is empty, and we received a new value from props
+        if (!this.state.changed && val == '' && this.props.value != '') {
+            //then update our value
+            val = this.props.value;
         }
         return (
             <div className={className}>
                 <label htmlFor={this.props.name}>{this.props.label}</label>
-                <input type={this.props.type} name={this.props.name} />
+                <input type={this.props.type} name={this.props.name} value={val} onChange={this.handleChange.bind(this)} />
             </div>
         );
+    }
+    
+    handleChange (e) {
+        //update value
+        this.setState({
+            changed: true,
+            value: e.target.value
+        });
+        //bubble event
+        if (typeof this.props.onChange == "function") {
+            this.props.onChange(e);
+        }
     }
 };
 //define prop types
@@ -27,7 +55,9 @@ Input.propTypes = {
     name: React.PropTypes.string,
     label: React.PropTypes.string,
     type: React.PropTypes.string,
-    float: React.PropTypes.string
+    value: React.PropTypes.string,
+    float: React.PropTypes.string,
+    onChange: React.PropTypes.func
 };
 //export component
 export { Input };
--- public/app/form/shared/Select.js
+++ public/app/form/shared/Select.js
@@ -9,29 +9,60 @@
 import React from 'react';
 //create Select react component
 var Select = class extends React.Component {
+    constructor (props) {
+        super(props);
+        //set initial state
+        this.state = {
+            changed: false,
+            value: this.props.value
+        };
+    }
+    
     render () {
+        //set value of our input
+        var val = this.state.value,
+            optionNodes = [];
         //loop options
-        var optionNodes = [];
         for (var opt in this.props.options) {
             optionNodes.push(
                 <option key={opt} val={opt}>{this.props.options[opt]}</option>
             );
         };
+        //if our state's value is empty, and we received a new value from props
+        if (!this.state.changed && !val && this.props.value != '') {
+            //then update our value
+            val = this.props.value;
+        }
         return (
             <div className="input-group">
                 <label htmlFor={this.props.name}>{this.props.label}</label>
-                <select name={this.props.name}>
+                <select name={this.props.name} value={val} onChange={this.handleChange.bind(this)}>
                     {optionNodes}
                 </select>
             </div>
         );
+    }
+    
+    handleChange (e) {
+        //update value
+        this.setState({
+            changed: true,
+            value: e.target.value
+        });
+        //bubble event
+        if (typeof this.props.onChange == "function") {
+            this.props.onChange(e);
+        }
     }
 };
 //define prop types
 Select.propTypes = {
     name: React.PropTypes.string,
     label: React.PropTypes.string,
-    options: React.PropTypes.object
+    options: React.PropTypes.object,
+    value: React.PropTypes.node,
+    float: React.PropTypes.string,
+    onChange: React.PropTypes.func
 };
 //export component
 export { Select };
